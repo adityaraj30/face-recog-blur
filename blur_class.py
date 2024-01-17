5ffe7f3271b8b2e564448077d4cfeff44f156859
Aditya Raj <71901647+adityaraj30@users.noreply.github.com>
2024-01-18 01:15:25 +0530

    Added Blur Class file

---
+++ blur_class.py
@@ -0,0 +1,82 @@
+import cv2 as cv
+import mediapipe as mp
+import numpy as np
+
+from facial_landmarks_src import FaceLandmarks
+
+i = 0
+# Load FaceLandmarks
+fl = FaceLandmarks()
+
+cap = cv.VideoCapture("video.mp4")
+ret, frame = cap.read()
+frame = cv.resize(frame, None, fx=0.5, fy=0.5)
+frame_copy = frame.copy()
+height, width, _ = frame.shape
+
+
+while True:
+    ret, frame = cap.read()
+    frame = cv.resize(frame, None, fx=0.5, fy=0.5)
+    frame_copy = frame.copy()
+    height, width, _ = frame.shape
+
+    # 1. Facial Landmark Detection (468 Facial Landmarks)
+    landmarks = fl.get_facial_landmarks(frame)
+
+    convexhull = cv.convexHull(landmarks)
+    print(convexhull)  # Polygon Formation
+
+    # print(landmarks)
+    # for i in range(0, 468):
+    #    pt = landmarks[i]
+    #    cv.circle(frame, (pt[0], pt[1]), 5, (0, 0, 255))
+
+    # cv.polylines(frame, [convexhull], True, (0,255,0), 3)
+
+    # 2. Face Blurring
+    mask = np.zeros((height, width), np.uint8)
+    # cv.polylines(mask, [convexhull], True, 255, 3)
+    cv.fillConvexPoly(mask, convexhull, 255)
+
+    # 3. Extracting Face
+    frame_copy = cv.blur(frame_copy, (27, 27))
+    face_extract = cv.bitwise_and(frame_copy, frame_copy, mask=mask)
+
+    # 4. Extract Background
+    background_mask = cv.bitwise_not(mask)
+    background = cv.bitwise_and(frame, frame, mask=background_mask)
+
+    # Final
+    result = cv.add(background, face_extract)
+
+    y = i
+    y = str(y)
+    y = y + ".jpeg"
+    i = i + 1
+    print(i)
+
+    #cv.imshow("Frame", frame)
+    #cv.imshow("Mask", mask)
+    #cv.imshow("Face Extract", face_extract)
+    #cv.imshow("Background Mask", background_mask)
+    #cv.imshow("Background", background)
+    #cv.imshow("FC", frame_copy)
+
+    if frame is not None:
+        cv.imshow("Final", result)
+        # video.write(result)
+
+
+    ret, frame = cap.read()
+
+    key = cv.waitKey(1)
+
+    if key == 27:
+        break
+
+# video.release()
+cap.release()
+cv.destroyAllWindows()
+
+
